b7a9eb5b21ed9b3daaa958441cd0401be6649c3f
Zeev Glozman <z@beame.io>
2017-04-30 22:23:50 -0400

    changed repo from ssh to https

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -1,33 +1,22 @@
-#include "sio_client.h"
-#include "boost/property_tree/ptree.hpp"
-#include "boost/property_tree/json_parser.hpp"
-#include <boost/foreach.hpp>
-
-#include <iostream>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <boost/asio.hpp>
-
-#include <boost/enable_shared_from_this.hpp>
-#include <boost/thread/mutex.hpp>
-#include <boost/make_shared.hpp>
-#include <boost/function.hpp>
-#include <boost/bind.hpp>
-#include <boost/thread.hpp>
-
-
-
-using namespace sio;
-using namespace std;
-using boost::asio::ip::tcp;
-namespace ip = boost::asio
-
 #include <stdio.h>
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
+#include <string>
+#include <boost/archive/iterators/binary_from_base64.hpp>
+#include <boost/archive/iterators/base64_from_binary.hpp>
+#include <boost/archive/iterators/transform_width.hpp>
+#include <boost/algorithm/string.hpp>
 
-bool generate_key()
+std::string decode64(const std::string &val) {
+    using namespace boost::archive::iterators;
+    using It = transform_width<binary_from_base64<std::string::const_iterator>, 8, 6>;
+    return boost::algorithm::trim_right_copy_if(std::string(It(std::begin(val)), It(std::end(val))), [](char c) {
+        return c == '\0';
+    });
+}
+
+
+bool generate_key(std::string pathPrefix)
 {
     int             ret = 0;
     RSA             *r = NULL;
@@ -36,7 +25,8 @@ bool generate_key()
 
     int             bits = 2048;
     unsigned long   e = RSA_F4;
-
+    std::string fullPathPublic = pathPrefix + "public_key.pem";
+    std::string fullPathPrivate = pathPrefix + "private_key.pem";
     // 1. generate rsa key
     bne = BN_new();
     ret = BN_set_word(bne,e);
@@ -51,14 +41,16 @@ bool generate_key()
     }
 
     // 2. save public key
-    bp_public = BIO_new_file("public.pem", "w+");
+
+    bp_public = BIO_new_file(fullPathPublic.c_str(), "w+");
     ret = PEM_write_bio_RSAPublicKey(bp_public, r);
     if(ret != 1){
         goto free_all;
     }
 
     // 3. save private key
-    bp_private = BIO_new_file("private.pem", "w+");
+
+    bp_private = BIO_new_file(fullPathPrivate.c_str(), "w+");
     ret = PEM_write_bio_RSAPrivateKey(bp_private, r, NULL, NULL, 0, NULL, NULL);
 
     // 4. free
